ff17dfe41481f72bc1dd1f941b330be5f60d5521
Denys Bilyk <bilko.dp@gmail.com>
2022-01-29 16:36:02 +0200

    Created and added styles to product component

--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -21,3 +21,8 @@ export const numSplitter = (num: number, ruSymbol = true): string => {
 		return res.concat(' ₽');
 	}
 };
+
+export const declOfNum = (number: number, titles: [string, string, string]): string => {
+	const cases = [2, 0, 1, 1, 1, 2];
+	return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+}
